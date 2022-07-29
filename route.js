1e31230bc1e30d4419e8200bb48f9e60b4d92f8b
Shubham Gautam <72097846+Shub-Gautam@users.noreply.github.com>
2022-07-29 21:17:16 +0530

    Fixing router.use

--- route.js
+++ route.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const v1Routes = require("./v1/route");
 
 router.use("/v1/flip", (req, res, next) => {
-  res.send("work");
+  v1Routes;
 });
 
 module.exports = router;
